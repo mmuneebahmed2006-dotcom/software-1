a4e0105a40f21b562ea05b035c28aec769ada997
mmuneebahmed2006-dotcom <mmuneebahmed2006@gmail.com>
2026-09-19 00:12:00 +0500

    Implement splash screen on application startup
    Added a splash screen that displays for 2.5 seconds before showing the main window.

--- main.cjs
+++ main.cjs
@@ -2,9 +2,20 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
+  const splash = new BrowserWindow({
+    width: 500,
+    height: 300,
+    frame: false,
+    alwaysOnTop: true,
+    resizable: false,
+  });
+  splash.loadFile(path.join(__dirname, 'splash.html'));
+
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
+    show: false,
+    title: 'Document Generator',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -12,6 +23,13 @@ function createWindow() {
   });
 
   win.loadFile(path.join(__dirname, '.output/public/index.html'));
+
+  win.once('ready-to-show', () => {
+    setTimeout(() => {
+      splash.close();
+      win.show();
+    }, 2500);
+  });
 }
 
 app.whenReady().then(createWindow);
